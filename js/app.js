91da82faf89940d0f5f2b5d2e509e62facc9283f
Jeni Tennison <jeni@theodi.org>
2015-11-21 14:21:15 +0000

    mp lookups too

--- js/app.js
+++ js/app.js
@@ -1,51 +1,59 @@
 $.get("constituencies.tsv", function (constituencyData) {
-  var lines = constituencyData.split("\n")
-  var constituencies = {}
-  for (i in lines) {
-    constituencies[lines[i].split("\t")[0]] = lines[i].split("\t")[1]
-  }
-
-  var constituencyLookup = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.whitespace,
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: Object.keys(constituencies)
-  });
-
-  // var mpLookup = new Bloodhound({
-  //   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
-  //   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  //   prefetch: '../data/nhl.json'
-  // });
-
-  $('#constituency').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'constituency',
-    source: constituencyLookup,
-    templates: {
-      header: '<h3 class="search-type">Constituencies</h3>'
+  $.get("MPs.tsv", function (mpData) {
+    var lines = constituencyData.split("\n")
+    var constituencies = {}
+    for (i in lines) {
+      constituencies[lines[i].split("\t")[0]] = lines[i].split("\t")[1]
     }
-  }
-  // ,
-  // {
-  //   name: 'mps',
-  //   display: 'constituency',
-  //   source: mpLookup,
-  //   templates: {
-  //     header: '<h3 class="search-type">MPs</h3>'
-  //   }
-  // }
-  );
 
-  $("#search").submit(function (event) {
-    $('#constituency').typeahead('close');
-    var search = $('#constituency').typeahead('val');
-    var code = constituencies[search];
-    window.location.hash = '#' + code;
-    return false;
+    var constituencyLookup = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: Object.keys(constituencies)
+    });
+
+    var lines = mpData.split("\n")
+    var mps = {}
+    for (i in lines) {
+      mps[lines[i].split("\t")[0]] = lines[i].split("\t")[1]
+    }
+
+    var mpLookup = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: Object.keys(mps)
+    });
+
+    $('#constituency').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: 'constituency',
+      source: constituencyLookup,
+      templates: {
+        header: '<h3 class="search-type">Constituencies</h3>'
+      }
+    },
+    {
+      name: 'mps',
+      display: 'constituency',
+      source: mpLookup,
+      templates: {
+        header: '<h3 class="search-type">MPs</h3>'
+      }
+    });
+
+    $("#search").submit(function (event) {
+      $('#constituency').typeahead('close');
+      var search = $('#constituency').typeahead('val');
+      var code = constituencies[search];
+      if (code == undefined) {
+        code = mps[search];
+      }
+      window.location.hash = '#' + code;
+      return false;
+    });
   });
-
 });
